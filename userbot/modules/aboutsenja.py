fe9b430f0b9e90051fc9a867cb905a9c4316743d
Itsmesenja <104503909+Itsmesenja@users.noreply.github.com>
2022-04-30 05:17:29 +0700

    Create aboutsenja.py

---
+++ userbot/modules/aboutsenja.py
@@ -0,0 +1,115 @@
+from time import sleep
+from userbot import CMD_HELP, CMD_HANDLER as cmd
+from userbot.utils import Xa_cmd
+
+
+@Xa_cmd(pattern="senjaintro1(?: |$)(.*)")
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    await typew.edit("**HAI!!**")
+    sleep(1.5)
+    await typew.edit("**Sebelumnya mau bilang**")
+    sleep(1.5)
+    await typew.edit("**TERIMAKASIH YANG SUDAH MENGGUNAKAN SENJAXSERBOT**")
+    sleep(2)
+    await typew.edit("**Maaf Jika buatan Saya tidak sebagus yang lain**")
+    sleep(2)
+    await typew.edit("**Tapi Saya Berusaha Untuk membuat kalian nyamam dengan buatan saya**")
+    sleep(2)
+    await typew.edit("**Bye the way**")
+    sleep(1.5)
+    await typew.edit("**Saya Asal Kota Jakarta**")
+    sleep(2)
+    await typew.edit("**Dan Umur Saya 20 Tahun**")
+    sleep(1.5)
+    await typew.edit("**Hmmmmm**")
+    sleep(1.5)
+    await typew.edit("**Udah gitu aja sih**")
+    sleep(1.5)
+    await typew.edit("**Lanjut ke Perintah yang lain yakk,**")
+    sleep(1.5)
+    await typew.edit("**Terimakasih**")
+
+
+@Xa_cmd(pattern="senjaintro2(?: |$)(.*)")
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    await typew.edit(
+        "**HALO!!!!Nama Saya Valentino (Senja), Saya pembuat SenjaxUserbot hehehe,  asal Kota Jakarta, yang satu kota yolah meet wkwk, Bang lu pasti jago coding?? Nope!!! Itu salah bgt, Gua cuma belajar otodidak, terus merhatiin orang orang, Ya pastinya gua juga dibantuin para suhu userbot, btw nih ya Kalo misalnya ada bug atau apapun itu kirim aja ya di gc support ku, atau butuh bantuan silahkan chat aja jgn sungkan**")
+
+
+@Xa_cmd(pattern="senjaintro3(?: |$)(.*)")
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    await typew.edit("**Okeee di perintah ini saya mau Sharing tentang First Time saya bermain telegram**")
+    sleep(1.5)
+    await typew.edit("**Pertama kali saya download Telegram hanya untuk download film, Drakor dan Anime**")
+    sleep(2)
+    await typew.edit("**Dan makin lama makin bosen, Dan akhirnya gua coba main anonymous bot**")
+    sleep(2)
+    await typew.edit("**Karena Anonymous makin aneh, u knowlah wkwkwk**")
+    sleep(1.5)
+    await typew.edit("**akhirnya gua memberanikan diri untuk os digc mutualan wkwk**")
+    sleep(1.5)
+    await typew.edit("**Ternyata asik anjg wkwkwk**")
+    sleep(1.5)
+    await typew.edit("**Terus gua mulai di ajak main ke gc Family dan diajak buat make tittle mereka**")
+    sleep(1.5)
+    await typew.edit("**Udah sih singkatnya gitu aja wkwkw, Ga ada yang menarik dari gua**")
+    sleep(1.5)
+    await typew.edit("**kalo awal gua main bot karena gua merasa asik sama bot Rose Dan music milik ka rexa**")
+    sleep(1.5)
+    await typew.edit("**Gua ga tau apa itu userbot awalnya, sampai ada anak aliansi mainin Clone pake userbot**")
+    sleep(1.5)
+    await typew.edit("**akhirnya gua tanya itu bot apa**")
+    sleep(1.5)
+    await typew.edit("**dan akhirnya gua dikasih tau, tapi dia ga ngasih gua tutornya, ya akhrinya gua dateng ke gc ka rexa dan di ajarin lah sm dia**")
+    sleep(2)
+    await typew.edit("**Intinya Repo Userbot pertama kali yg gua pake itu Xa - Userbot**")
+    sleep(1.5)
+    await typew.edit("**Repo Music yang pertama kali gua pake itu Yukki**")
+    sleep(1.5)
+    await typew.edit("**itu aja sih makasih wkwkwk**")
+
+
+@Xa_cmd(pattern="senjaintro3(?: |$)(.*)")
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    await typew.edit("**DISINI GA ADA APA APA WKWKWK**")
+
+
+@Xa_cmd(pattern="senjaproject(?: |$)(.*)")
+async def project_senja(wannasee):
+    await wannasee.edit(
+        f"⌬ **sᴇɴᴊᴀ ᴍᴜsɪᴄ       :** [ᴋʟɪᴋ ᴅɪsɪɴɪ](https://t.me/sajikuxrobot)\n"
+        f"⌬ **sᴇɴᴊᴀ sᴛʀɪɴɢ ʀᴏʙᴏᴛ :** [ᴋʟɪᴋ ᴅɪsɪɴɪ](https://t.me/senjastring_bot)\n"
+        f"⌬ **sᴇᴍᴜᴀ ʙᴏᴛ sᴇʟᴀʟᴜ sᴀʏᴀ ᴜᴘᴅᴀᴛᴇ\n"
+        f"⌬ **ᴘᴇʀɪᴋsᴀ ᴄʜᴀɴɴᴇʟ : [sᴇɴᴊᴀ ᴘʀᴏᴊᴇᴄᴛ](https://t.me/senjaprojectt)")
+
+
+@Xa_cmd(pattern="senjabio(?: |$)(.*)")
+async def bio_senja(biosenja):
+    await bioxa.edit(
+        f"👨‍💻 **Nama        : Valentino (Senja)\n"
+        f"📅 **TTL         : Jakata, 20 September 2000\n"
+        f"🎸 **Genre Music : Dj, Metal, Rap, Lofi\n"
+        f"👓 **Hobi        : Musik, Film, Anime\n"
+        f"🌍 **Instagram   :**[ᴋʟɪᴋ ᴀᴊᴀ](https://www.instagram.com/sajikuhibiniuuu/)\n"
+        f"🎵 **Telegram    :**[ᴋʟɪᴋ ᴀᴊᴀ](https://t.me/Itsmesenjaaah)\n"
+        f"Untuk lebih jelasnya tanya aja sendiri")
+
+
+CMD_HELP.update(
+    {
+        "aboutsenja": f"**Plugin : **`aboutsenja`\
+        \n\n    **Perintah :** `{cmd}senjaintro1`\
+        \n⌬    **Fungsi : **Perkenalan tentang owner SenjaxUserbot.\
+        \n\n    **Perintah :** `{cmd}senjaintro2`\
+        \n⌬    **Fungsi : **Perkenalan tentang owner SenjaxUserbot 2.\
+        \n\n    **Perintah :** `{cmd}senjaintro3`\
+        \n⌬    **Fungsi : **Perkenalan Tentang Owner Senja x Userbot 3.\
+        \n\n    **Perintah :** `{cmd}senjaproject`\
+        \n⌬    **Fungsi : **Semua Tentang Project Bot Senja.\
+        \n\n    **Perintah :** `{cmd}senjabio`\
+        \n⌬    **Fungsi : **Biodata Owner Senja xUserbot."
+    })
